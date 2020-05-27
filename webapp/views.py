7594120fdbd7eb3500fe8049cccd44c8d98b2104
wfchiang <weifan.wf@gmail.com>
2020-05-27 16:52:58 -0400

    add unit tests

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 
+from . import ss2json
+
 # Ping endpoint 
 def ping (request): 
+    spreadsheetsId = '1brRpP3_vCuGHAVkA6HkKxLm4DsfnNDNBQVWQCv9prkw'
+    dataRange = 'A2:B3'
+    gssService = ss2json.getGoogleSpreadsheetsService() 
+    values = ss2json.loadTheTableFromGoogleSpreadsheets(
+        spreadsheetsService=gssService, 
+        spreadsheetsId=spreadsheetsId,
+        sheetId=None)
+    print ('>> ' + str(values))
     return HttpResponse("Ping!")
